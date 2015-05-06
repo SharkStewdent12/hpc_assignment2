4de72c7407c9dd83920b24893db8ebc9bdfcae15
Mark Durrheim <570169@students.wits.ac.za>
2015-05-06 13:12:00 +0200

    Marix generation added.

--- matrix.cu
+++ matrix.cu
@@ -1,3 +1,7 @@
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+
+
 typedef struct {
 	int width;
 	int height;
@@ -14,6 +18,26 @@ void applyConvolution_parallel(Matrix sourceMatrix, Matrix kernel, Matrix output
 
 void populateRandomMatrix( Matrix matrix ) {
 
+	srand (time(NULL)); //initialise random seed
 
+	int rowStart = (matrix.height-1) * matrix.width;
+	//set top and bottom rows to be all zero, for border
+	for (int col = 0, col < matrix.width, col++) {
+		matrix.elements[col] = 0;
+		matrix.elements[rowStart + col] = 0;
+	}//for col
+
+	//populate with random integers
+	for (int row = 1; row < matrix.height-1; row++){ //already done first and last rows
+
+		rowStart = row*matrix.width;
+		matrix.elements[rowStart] = 0; //first column to 0
+		matrix.elements[rowStart+matrix.width-1] = 0; //last column to 0
+
+		for (int col = 1, col < matrix.width-1, col++) { //first and last col are for border
+			matrix.elements[rowStart+col] = rand() % 256; //integer between 0 and 255
+		}//for col
+
+	}//for row
 
 }//function populateRandomMatrix
