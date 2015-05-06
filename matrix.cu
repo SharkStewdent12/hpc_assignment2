3db12b2ba329f5856fc73319f32ef3846dd1c407
Mark Durrheim <570169@students.wits.ac.za>
2015-05-06 12:42:21 +0200

    refactoring structure

---
+++ matrix.cu
@@ -0,0 +1,19 @@
+typedef struct {
+	int width;
+	int height;
+	float* elements;
+} Matrix;
+
+void applyConvolution_serial(Matrix sourceMatrix, Matrix kernel, Matrix outputMatrix) {
+	//TO-DO
+}//function applyConvolution_serial
+
+void applyConvolution_parallel(Matrix sourceMatrix, Matrix kernel, Matrix outputMatrix) {
+	//TO-DO
+}//function applyConvolution_parallel
+
+void populateRandomMatrix( Matrix matrix ) {
+
+
+
+}//function populateRandomMatrix
